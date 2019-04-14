b7fbc2654201e43f029b994abf2404e38cb19566
Oleg Malyshev <prostooleg1@gmail.com>
2019-04-14 22:09:31 +0300

    tests

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -3,6 +3,9 @@ import org.junit.Test;
 
 public class MainClassTest {
 
+    private int class_number = 20;
+    private String class_string = "Hello, world";
+
 
 
     @Test
@@ -11,8 +14,29 @@ public class MainClassTest {
 
     }
 
+
+    @Test
+    public void testGetClassNumber(){
+        Assert.assertTrue("метод getClassNumber возвращает больше 20!",this.getClassNumber() <= 20);
+
+    }
+
+
+    @Test
+    public void testGetClassString(){
+        Assert.assertTrue("Метод testGetClassString не содержит слова Hello",this.getClassString().contains("Hello"));
+    }
+
     public  int getLocalNumber(){
         return 14;
     }
 
+
+    public int getClassNumber(){
+        return this.class_number;
+    }
+
+    public String getClassString(){
+        return class_string;
+    }
 }
